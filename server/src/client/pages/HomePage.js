48bb29033cce0af4912a2a302b09c32e399ec32e
Pavilion <tanv.mhn@gmail.com>
2021-03-21 22:23:45 +0600

    initial commit

---
+++ server/src/client/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="center-align" style={{ marginTop: '200px' }}>
+      <h3>Welcome</h3>
+    </div>
+  );
+};
+
+export default {
+  component: Home
+};
